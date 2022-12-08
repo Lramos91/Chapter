190d1da8215dcc5b8f7e582a3ef2d6ddea950a68
Lramos91 <lucas.ramosdavid@gmail.com>
2022-12-08 14:04:14 -0300

    alterações

---
+++ Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,30 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) :base(options)
+        {
+
+        }
+
+        //vamos utilizar esse metodo para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(optionsBuilder.IsConfigured)
+            {
+                //cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-8BI4M7R\\SQLEXPRESS ; initial catalog = Chapter ; Integrated Security = true");
+            }
+        }
+
+        //dbset representa as entidades que serão utilizadas nas operações de leitura,criação, atualização e exclusão.
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
